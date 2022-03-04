290ec600b3bc9aa319c0416585ea8a74f0ddae1d
SandeMiriam <miriamsandeoyuga@gmail.com>
2022-03-04 16:45:59 +0300

    interpolation

---
+++ src/main.kt
@@ -0,0 +1,29 @@
+fun main(){
+name()
+statement()
+ mylength()
+    output()
+}
+fun name(){
+    var school="AkiraChix"
+    println(school[0].toString()+school[2]+school[3])
+}
+fun statement(){
+    var name="hi my name is sande"
+    var age="am 19 years old"
+    println(name+" "+age)
+}
+fun mylength(){
+    var name="overwhelmed"
+    println(name.length)
+}
+fun output(){
+    var name="faith"
+    var x=name
+    if(name ==name){
+    println("Thats me!")}
+    else
+        println("I don't know who that is")
+
+
+}
